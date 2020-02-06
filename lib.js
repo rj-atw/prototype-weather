7535d1f85e086ad3b35fc4cf6b224736b196e78a
rj <atwal.rj@gmail.com>
2020-02-06 02:15:37 -0800

    General cleanup and classification of test cases. Next commit will refactoring to prepare to call the weather API

--- lib.js
+++ lib.js
@@ -1,7 +1,7 @@
 const openWeatherLocation = require('./current.city.list.min.json') 
 const tzlookup = require('tz-lookup')
 
-class LocationError {
+class FindingLocationError {
   constructor(error, location, estimate) {
     this.error = error
     this.location = location	  
@@ -9,26 +9,18 @@ class LocationError {
   }
 }
 
-function latinise(word) {
-  return word.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-}
-
-function getTimeFromLocation(currentTime, location) {
-  // openweathermap database has latinised (ascii) version of location name
-  // e.g. "São Paulo" -> "Sao Paulo"
-  location = latinise(location)
-  
+function getTimeFromLocation(currentTime, location) { 
   var locations = openWeatherLocation
-   .filter( (cityJson) => cityJson.name === location )
+   .filter( (cityJson) => cityJson.name.localeCompare(location, 'en', {'sensitivity': 'base'}) == 0 )
    //Limiting to a single layer of the geo hierarchy used by openweathermap 
    .filter( (cityJson) => cityJson.geoname.cl === 'P')	
   
   if(locations.length == 0) {
-    return new LocationError("Location is unknown by the weather database",	location, null)
+    return new FindingLocationError("Location is unknown by the weather database",location, null)
   }
 
   if(locations.length > 1) {
-    return new LocationError("Location is ambiguous", location, locations.splice(0,1))
+    return new FindingLocationError("Location is ambiguous", location, locations.splice(0,1))
   }
 
   return locations
@@ -38,4 +30,4 @@ function getTimeFromLocation(currentTime, location) {
 }
 
 exports.getTimeFromLocation = getTimeFromLocation  
-exports.LocationError = LocationError
+exports.FindingLocationError = FindingLocationError
